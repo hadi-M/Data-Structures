0bb4d864b4691aa7bafebcabb30a94124e91e24a
modares <modareshadi@gmail.com>
2020-07-14 23:05:40 -0700

    haven't started module 2 assignment yet

--- queue/queue.py
+++ queue/queue.py
@@ -14,7 +14,7 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 
-
+# QUEUE IMPLEMENTATION WITH PYTHON LISTS
 class Queue:
     def __init__(self):
         self.size = 0
@@ -25,8 +25,11 @@ class Queue:
 
     def enqueue(self, value):
         self.storage.append(None)
+
+        # shift all of the storage:
         for i in range(len(self.storage)-1, 0, -1):
             self.storage[i] = self.storage[i-1]
+
         self.storage[0] = value
         self.size += 1
 
@@ -38,3 +41,64 @@ class Queue:
             del self.storage[-1]
             self.size -= 1
             return return_value
+
+
+# QUEUE IMPLEMENTATION WITH SINGLY LINKED LIST
+# import sys
+# sys.path.append("..")
+
+# from singly_linked_list.singly_linked_list import LinkedList
+
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.storage.add_to_tail(value)
+#         self.size += 1
+
+#     def dequeue(self):
+#         return_value = self.storage.remove_head()
+#         if return_value != None:
+#             self.size -= 1
+#         return return_value
+
+
+# QUEUE IMPLEMENTATION WITH 2 STACK
+import sys
+sys.path.append("..")
+from stack.stack import Stack
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = Stack()
+        self.temp_storage = Stack()
+
+    def __len__(self):
+        return self.size
+
+    def enqueue(self, value):
+        self.storage.push(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size == 0:
+            return None
+
+        for i in range(self.size-1):
+            self.temp_storage.push(self.storage.pop())
+        
+        return_value = self.storage.pop()
+        
+        for i in range(self.size-1):
+            self.storage.push(self.temp_storage.pop())
+
+        self.size -= 1
+        
+        return return_value
